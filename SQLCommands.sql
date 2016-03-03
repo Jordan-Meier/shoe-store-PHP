8f0c0a22bfc05461851f64f29f7ad7f285b11537
Jordan Meier <jbirdmeier@gmail.com>
2016-03-03 15:00:00 -0800

    add a SQL commands file

---
+++ SQLCommands.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE shoes;
+
+USE shoes;
+
+CREATE TABLE stores
+(
+  id serial PRIMARY KEY,
+  name VARCHAR(255),
+  location VARCHAR (255),
+);
+
+CREATE TABLE brands
+(
+  id serial PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE stores_brands
+(
+  id serial PRIMARY KEY,
+  store_id INT,
+  brand_id INT
+);
