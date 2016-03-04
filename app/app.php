5fa7acc30682aaec502f7fd8ca79c2b7173fff86
Jordan Meier <jbirdmeier@gmail.com>
2016-03-04 10:06:53 -0800

    add get routes for index page, brands page and stores page

--- app/app.php
+++ app/app.php
@@ -1,14 +1,33 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/ClassName.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
 
     $app = new Silex\Application();
+    // $app['debug'] = true;
+
+    $server = 'mysql:host=localhost;dbname=shoes';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/stores", function() use ($app) {
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->get("/brands", function() use ($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
     });
 
 
