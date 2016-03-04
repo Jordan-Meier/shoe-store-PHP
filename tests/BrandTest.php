5c4217660bc7fa677cd3b99e9cbc052202daef8b
Jordan Meier <jbirdmeier@gmail.com>
2016-03-04 10:02:09 -0800

    fix spacing on test pages for easy readability

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -6,10 +6,12 @@
 */
     require_once "src/Brand.php";
     require_once "src/Store.php";
+
     $server = 'mysql:host=localhost;dbname=shoes_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
+    
     class BrandTest extends PHPUnit_Framework_TestCase
     {
         protected function tearDown()
