a3b613c94635707d408d3541d7aaa0858730e1a9
Jordan Meier <jbirdmeier@gmail.com>
2016-03-04 09:54:20 -0800

    add and pass delete()/getBrands()/addBrand() methods to Store class

--- src/Store.php
+++ src/Store.php
@@ -39,49 +39,79 @@
 
         function save()
         {
-              $GLOBALS['DB']->exec("INSERT INTO stores (store_name, location) VALUES ('{$this->getStoreName()}', '{$this->getLocation()}');");
-              $this->id = $GLOBALS['DB']->lastInsertId();
+            $GLOBALS['DB']->exec("INSERT INTO stores (store_name, location) VALUES  ('{$this->getStoreName()}', '{$this->getLocation()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
-       {
-           $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
-           $stores = array();
-           foreach($returned_stores as $store) {
-               $store_name = $store['store_name'];
-               $location = $store['location'];
-               $id = $store['id'];
-               $new_store = new Store($store_name, $location, $id);
-               array_push($stores, $new_store);
-           }
-           return $stores;
-       }
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            $stores = array();
+            foreach($returned_stores as $store) {
+                $store_name = $store['store_name'];
+                $location = $store['location'];
+                $id = $store['id'];
+                $new_store = new Store($store_name, $location, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
 
-       static function deleteAll()
-       {
-         $GLOBALS['DB']->exec("DELETE FROM stores;");
-       }
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
 
-       static function find($search_id)
-       {
-           $found_store = null;
-           $stores = Store::getAll();
-           foreach($stores as $store) {
-               $store_id = $store->getId();
-               if ($store_id == $search_id) {
-                 $found_store = $store;
-               }
-           }
-           return $found_store;
-       }
+        static function find($search_id)
+        {
+            $found_store = null;
+            $stores = Store::getAll();
+            foreach($stores as $store) {
+                $store_id = $store->getId();
+                if ($store_id == $search_id) {
+                    $found_store = $store;
+                }
+            }
+            return $found_store;
+        }
 
-       function update($new_store_name, $new_location)
-     {
-         $GLOBALS['DB']->exec("UPDATE stores SET store_name = '{$new_store_name}', location = '{$new_location}' WHERE id = {$this->getId()};");
-         $this->setStoreName($new_store_name);
-         $this->setLocation($new_location);
+        function update($new_store_name, $new_location)
+        {
+            $GLOBALS['DB']->exec("UPDATE stores SET store_name = '{$new_store_name}', location =    '{$new_location}' WHERE id = {$this->getId()};");
+            $this->setStoreName($new_store_name);
+            $this->setLocation($new_location);
 
-     }
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM stores_brands WHERE store_id = {$this->getId()};");
+        }
+
+        function addBrand($brand)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores_brands (store_id, brand_id) VALUES ({$this->getId()}, {$brand->getId()});");
+        }
+
+        function getBrands()
+        {
+            $query = $GLOBALS['DB']->query(
+                "SELECT brands.*
+                FROM stores
+                JOIN stores_brands ON (stores.id = stores_brands.store_id)
+                JOIN brands ON (stores_brands.brand_id = brands.id)
+                WHERE stores.id = {$this->getId()};"
+            );
+            $brands = [];
+            foreach($query as $brand){
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($name, $id);
+                $brands[] = $new_brand;
+            }
+            return $brands;
+        }
 
     }
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -14,8 +14,8 @@
     {
         protected function tearDown()
         {
-          Brand::deleteAll();
-          Store::deleteAll();
+            Brand::deleteAll();
+            Store::deleteAll();
         }
 
         function testSave()
@@ -60,7 +60,7 @@
             $store_name2 = "Payless";
             $location2 = "Olympia, WA";
             $test_store2 = new Store($store_name2, $location2);
-            $test_store->save();
+            $test_store2->save();
             //Act
             Store::deleteAll();
             //Assert
@@ -69,40 +69,95 @@
         }
 
         function testFind()
-       {
-           //Arrange
-           $store_name = "REI";
-           $location = "Seattle, WA";
-           $test_store = new Store($store_name, $location);
-           $test_store->save();
+        {
+            //Arrange
+            $store_name = "REI";
+            $location = "Seattle, WA";
+            $test_store = new Store($store_name, $location);
+            $test_store->save();
 
-           $store_name2 = "Payless";
-           $location2 = "Olympia, WA";
-           $test_store2 = new Store($store_name2, $location2);
-           $test_store->save();
-           //Act
-           $result = Store::find($test_store->getId());
-           //Assert
-           $this->assertEquals($test_store, $result);
-       }
+            $store_name2 = "Payless";
+            $location2 = "Olympia, WA";
+            $test_store2 = new Store($store_name2, $location2);
+            $test_store2->save();
+            //Act
+            $result = Store::find($test_store->getId());
+            //Assert
+            $this->assertEquals($test_store, $result);
+        }
 
-       function testUpdate()
-      {
-          //Arrange
-          $store_name = "REI";
-          $location = "Seattle, WA";
-          $test_store = new Store($store_name, $location);
-          $test_store->save();
+        function testUpdate()
+        {
+            //Arrange
+            $store_name = "REI";
+            $location = "Seattle, WA";
+            $test_store = new Store($store_name, $location);
+            $test_store->save();
 
-          $new_store_name = "Imeldas";
-          $new_location = "Portland, OR";
-          //Act
-          $test_store->update($new_store_name, $new_location);
-          //Assert
-          $this->assertEquals(["Imeldas", "Portland, OR"], [$test_store->getStoreName(), $test_store->getLocation()]);
-      }
+            $new_store_name = "Imeldas";
+            $new_location = "Portland, OR";
+            //Act
+            $test_store->update($new_store_name, $new_location);
+            //Assert
+            $this->assertEquals(["Imeldas", "Portland, OR"], [$test_store->getStoreName(),   $test_store->getLocation()]);
+        }
 
+        function testDelete()
+        {
+            //Arrange
+            $store_name = "REI";
+            $location = "Seattle, WA";
+            $test_store = new Store($store_name, $location);
+            $test_store->save();
 
+            $name = "Chacos";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            //Act
+            $test_store->addBrand($test_brand);
+            $test_store->delete();
+            //Assert
+            $this->assertEquals([], $test_brand->getStores());
+        }
+
+        function test_getBrands()
+        {
+            //Arrange
+            $store_name = "REI";
+            $location = "Seattle, WA";
+            $test_store = new Store($store_name, $location);
+            $test_store->save();
+
+            $name = "Vans";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+
+            $name2 = "Chacos";
+            $test_brand2 = new Brand($name2);
+            $test_brand2->save();
+            //Act
+            $test_store->addBrand($test_brand);
+            $test_store->addBrand($test_brand2);
+            //Assert
+            $this->assertEquals($test_store->getBrands(), [$test_brand, $test_brand2]);
+        }
+        function test_addBrand()
+        {
+            //Arrange
+            $store_name = "REI";
+            $location = "Seattle, WA";
+            $test_store = new Store($store_name, $location);
+            $test_store->save();
+
+            $name = "Chacos";
+            $test_brand = new Brand($name);
+            $test_brand->save();
+            //Act
+            $test_store->addBrand($test_brand);
+            //Assert
+            $this->assertEquals($test_store->getBrands(), [$test_brand]);
+        }
 
     }
 ?>
