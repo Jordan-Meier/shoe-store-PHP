2c83858d4caa55932f6f1e49524aa26ccf6f88c3
Jordan Meier <jbirdmeier@gmail.com>
2016-03-04 08:46:09 -0800

    fix sql statements so that their syntax is correct

--- SQLCommands.sql
+++ SQLCommands.sql
@@ -2,22 +2,8 @@ CREATE DATABASE shoes;
 
 USE shoes;
 
-CREATE TABLE stores
-(
-  id serial PRIMARY KEY,
-  store_name VARCHAR(255),
-  location VARCHAR (255),
-);
+CREATE TABLE stores (id serial PRIMARY KEY, store_name VARCHAR(255), location VARCHAR (255));
 
-CREATE TABLE brands
-(
-  id serial PRIMARY KEY,
-  name VARCHAR(255)
-);
+CREATE TABLE brands (id serial PRIMARY KEY, name VARCHAR(255));
 
-CREATE TABLE stores_brands
-(
-  id serial PRIMARY KEY,
-  store_id INT,
-  brand_id INT
-);
+CREATE TABLE stores_brands (id serial PRIMARY KEY, store_id INT, brand_id INT);
